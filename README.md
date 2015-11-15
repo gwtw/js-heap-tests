48b0aac34e5bb8af825757d80a429ee936c90c50
Daniel Imms <tyriar@tyriar.com>
2015-11-14 18:33:50 -0800

    Initial import from js-binary-heap

---
+++ README.md
@@ -0,0 +1,3 @@
+## js-heap-tests
+
+A set of tests for heap data structures.
---
+++ is-empty-tests.js
@@ -0,0 +1,11 @@
+module.exports.run = function (test, Heap) {
+  test('should return whether the heap is empty', t => {
+    var heap = new Heap();
+    t.true(heap.isEmpty());
+    heap.insert(1, null);
+    t.false(heap.isEmpty());
+    heap.extractMinimum();
+    t.true(heap.isEmpty());
+    t.end();
+  });
+};
